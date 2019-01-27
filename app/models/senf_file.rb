3039374680a002f652a2d06c7bd9a3df26df7030
Takaaki Muta <takaaki.muta@hamitech.jp>
2019-01-27 21:01:14 +0900

    実装中

--- app/models/senf_file.rb
+++ app/models/senf_file.rb
@@ -1,2 +1,13 @@
 class SenfFile < ApplicationRecord
+
+  # カード情報が無いか検索する # delayed jobで呼ぶ
+  def scan
+    resource = Tika::Resource.new(path)
+    puts resource.text
+  end
+
+
+  private
+  def scanner(extension)
+  end
 end
---
+++ config/initializers/tika.rb
@@ -0,0 +1,1 @@
+Tika::App.path = Rails.root.join('tika', 'tika-app-1.9.jar').to_s
